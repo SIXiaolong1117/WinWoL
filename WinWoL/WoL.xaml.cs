821293356ddc7a6fc94dcf3c6963b05c3f81f398
SI_Xiaolong <sixiaolong2018@outlook.com>
2023-04-15 16:40:53 +0800

    代码微调，健壮性提高。完善注释。

--- WinWoL/WoL.xaml.cs
+++ WinWoL/WoL.xaml.cs
@@ -81,8 +81,10 @@ namespace WinWoL
         // 主刷新函数
         private void refresh(string ConfigIDNum)
         {
+            // 读取localSettings中存储的字符串
             string configInner = localSettings.Values["ConfigID" + ConfigIDNum] as string;
 
+            // 初始化界面元素
             ConfigName.Text = "配置别名：";
             MacAddress.Text = "主机 Mac：";
             IpAddress.Text = "WoL 主机地址：";
@@ -96,8 +98,15 @@ namespace WinWoL
             WoLConfig.IsEnabled = false;
             RDPConfig.IsEnabled = false;
 
+            // 如果字符串不为空
             if (configInner != null)
             {
+                // 修改界面UI可用性和文字显示
+                AddConfig.Content = "修改配置";
+                DelConfig.IsEnabled = true;
+                WoLConfig.IsEnabled = true;
+
+                // 分割字符串
                 string[] configInnerSplit = configInner.Split(',');
                 // 传入的字符串结构：
                 // configName.Text + "," + macAddress.Text + ","
@@ -111,6 +120,9 @@ namespace WinWoL
                 string rdpIpAddress = configInnerSplit[5];
                 string rdpPort = configInnerSplit[6];
 
+                // 更新RDP的命令行
+                localSettings.Values["mstscCMD"] = "mstsc /v:" + rdpIpAddress + ":" + rdpPort + ";";
+
                 // 如果不是广播地址，则显示IP或域名。
                 // 如果是广播地址，则显示“向 LAN 网络广播”
                 string ipAddressDisplay = ipAddress;
@@ -118,9 +130,6 @@ namespace WinWoL
                 {
                     ipAddressDisplay = "向 LAN 网络广播";
                 }
-
-                // 更新RDP的命令行
-                localSettings.Values["mstscCMD"] = "mstsc /v:" + rdpIpAddress + ":" + rdpPort + ";";
 
                 // 如果开启RDP
                 if (rdpIsOpen == "True")
@@ -136,6 +145,7 @@ namespace WinWoL
                     RefConfig.IsEnabled = true;
                     RDPConfig.IsEnabled = true;
                 }
+                // 没有开启RDP
                 else
                 {
                     ConfigName.Text = "配置别名：" + configName;
@@ -149,19 +159,16 @@ namespace WinWoL
                     RefConfig.IsEnabled = false;
                     RDPConfig.IsEnabled = false;
                 }
-
-                AddConfig.Content = "修改配置";
-                DelConfig.IsEnabled = true;
-                WoLConfig.IsEnabled = true;
             }
         }
         // 以UDP协议发送MagicPacket
         public void sendMagicPacket(string macAddress, string domain, int port)
         {
-            // 将string分割为十六进制字符串数组
-            string s = macAddress;
+            // 将传入的Mac地址字符串分割为十六进制字符串数组
             // hexStrings = {"11", "22", "33", "44", "55", "66"}
+            string s = macAddress;
             string[] hexStrings = s.Split(':');
+
             // 创建一个byte数组
             byte[] bytes = new byte[hexStrings.Length];
             // 遍历字符串数组，将每个字符串转换为byte值，并存储到byte数组中
@@ -170,12 +177,14 @@ namespace WinWoL
                 // 使用16作为基数表示十六进制格式
                 bytes[i] = Convert.ToByte(hexStrings[i], 16);
             }
+            // 将MAC地址转换为字节数组：byte[] mac = new byte[] { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66 };
+            byte[] mac = bytes;
+
             // 创建一个UDP Socket对象
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
             // 设置需要广播数据
             socket.EnableBroadcast = true;
-            // 将MAC地址转换为字节数组：byte[] mac = new byte[] { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66 };
-            byte[] mac = bytes;
+
             // 创建一个魔术包
             byte[] packet = new byte[17 * 6];
             // 填充前6个字节为0xFF
@@ -185,10 +194,13 @@ namespace WinWoL
             for (int i = 1; i <= 16; i++)
                 for (int j = 0; j < 6; j++)
                     packet[i * 6 + j] = mac[j];
+
             // 获取IP地址
             IPAddress ip = domain2ip(domain);
+
             // 发送数据
             socket.SendTo(packet, new IPEndPoint(ip, port));
+
             // 关闭Socket对象
             socket.Close();
         }
@@ -229,16 +241,22 @@ namespace WinWoL
             // 判断replay，是否连通
             if (reply.Status == IPStatus.Success)
             {
+                // 如果连通，尝试与指定端口通信
                 var client = new TcpClient();
                 if (!client.ConnectAsync(ipAddress, port).Wait(500))
                 {
-                    //连接失败
+                    // 与指定端口通信失败
                     return "端口连接失败";
                 }
-                return reply.RoundtripTime.ToString() + " ms";
+                else
+                {
+                    // 与指定端口通信成功，计算RTT并返回
+                    return reply.RoundtripTime.ToString() + " ms";
+                }
             }
             else
             {
+                // 无法联通
                 return "无法联通";
             }
 
@@ -246,63 +264,84 @@ namespace WinWoL
         // 根据配置文件，调用发送MagicPacket
         private void WoLPC(string ConfigIDNum)
         {
+            // 读取localSettings中的字符串
             string configInner = localSettings.Values["ConfigID" + ConfigIDNum] as string;
+            // 如果字符串非空
             if (configInner != null)
             {
+                // 分割字符串
                 string[] configInnerSplit = configInner.Split(',');
                 // configName.Text + "," + macAddress.Text + "," + ipAddress.Text + ":" + ipPort.Text;
                 string macAddress = configInnerSplit[1];
                 string ipAddress = configInnerSplit[2];
                 string ipPort = configInnerSplit[3];
+
+                // 尝试发送Magic Packet，成功打开已发送弹窗
                 try
                 {
                     sendMagicPacket(macAddress, ipAddress, int.Parse(ipPort));
                     MagicPacketIsSendTips.IsOpen = true;
                 }
+                // 失败打开发送失败弹窗
                 catch
                 {
                     MagicPacketNotSendTips.IsOpen = true;
                 }
-
             }
         }
         // 唤起mstsc函数
         private void RDPPCChildThread()
         {
+            // 创建一个新的进程
             Process process = new Process();
+            // 指定运行PowerShell
             process.StartInfo.FileName = "PowerShell.exe";
+            // 参数为唤起mstsc的参数
+            // 他保存在localSettings中，随主刷新函数刷新
             process.StartInfo.Arguments = localSettings.Values["mstscCMD"] as string;
             //是否使用操作系统shell启动
             process.StartInfo.UseShellExecute = false;
             //是否在新窗口中启动该进程的值 (不显示程序窗口)
             process.StartInfo.CreateNoWindow = true;
+            // 进程开始
             process.Start();
+            // 等待执行结束
             process.WaitForExit();
+            // 进程关闭
             process.Close();
         }
         // 删除配置
         private void delConfig(string ConfigIDNum)
         {
+            // 清空指定ConfigIDNum的localSettings
             localSettings.Values["ConfigID" + ConfigIDNum] = null;
         }
         // Ping RDP主机端口
         private void PingRDPRef(string ConfigIDNum)
         {
+            // 从localSettings中读取字符串
             string configInner = localSettings.Values["ConfigID" + ConfigIDNum] as string;
-            string[] configInnerSplit = configInner.Split(',');
-            // 传入的字符串结构：
-            // configName.Text + "," + macAddress.Text + ","
-            // + ipAddress.Text + "," + ipPort.Text + ","
-            // + rdpIsOpen.IsOn + "," + rdpIpAddress.Text + "," + rdpIpPort;
-            string rdpIpAddress = configInnerSplit[5];
-            string rdpPort = configInnerSplit[6];
-            try
+            // 如果字符串非空
+            if (configInner != null)
             {
-                RDPPing.Text = "RDP 端口延迟：" + PingTest(rdpIpAddress, int.Parse(rdpPort)).ToString();
-            }
-            catch
-            {
-                RDPPing.Text = "RDP 端口延迟：无法联通";
+                // 分割字符串
+                string[] configInnerSplit = configInner.Split(',');
+                // 传入的字符串结构：
+                // configName.Text + "," + macAddress.Text + ","
+                // + ipAddress.Text + "," + ipPort.Text + ","
+                // + rdpIsOpen.IsOn + "," + rdpIpAddress.Text + "," + rdpIpPort;
+                string rdpIpAddress = configInnerSplit[5];
+                string rdpPort = configInnerSplit[6];
+
+                // 检查RDP主机端口是否可以Ping通
+                try
+                {
+                    RDPPing.Text = "RDP 端口延迟：" + PingTest(rdpIpAddress, int.Parse(rdpPort)).ToString();
+                }
+                catch
+                {
+                    RDPPing.Text = "RDP 端口延迟：无法联通";
+                }
             }
         }
 
@@ -312,17 +351,26 @@ namespace WinWoL
         {
             refresh(configNum.SelectedItem.ToString());
             localSettings.Values["configNum"] = configNum.SelectedItem;
+
+            //关闭所有弹出窗口
+            MagicPacketIsSendTips.IsOpen = false;
+            MagicPacketNotSendTips.IsOpen = false;
         }
         // 添加/修改配置按钮点击
         private async void AddConfigButton_Click(object sender, RoutedEventArgs e)
         {
             string ConfigIDNum = configNum.SelectedItem.ToString();
+            // 将ConfigIDTemp所存储的字符串设置为当前配置所存储的字符串
+            // 这样可以实现“修改”的操作
             localSettings.Values["ConfigIDTemp"] = localSettings.Values["ConfigID" + ConfigIDNum];
 
+            // 创建一个新的dialog对象
             AddConfigDialog configDialog = new AddConfigDialog();
 
+            // 对此dialog对象进行配置
             configDialog.XamlRoot = this.XamlRoot;
             configDialog.Style = Microsoft.UI.Xaml.Application.Current.Resources["DefaultContentDialogStyle"] as Style;
+            // 根据内容有无来决定PrimaryButton的内容
             if (AddConfig.Content.ToString() == "修改配置")
             {
                 configDialog.PrimaryButtonText = "修改";
@@ -332,13 +380,18 @@ namespace WinWoL
                 configDialog.PrimaryButtonText = "添加";
             }
             configDialog.CloseButtonText = "关闭";
+            // 默认按钮为PrimaryButton
             configDialog.DefaultButton = ContentDialogButton.Primary;
 
+            // 异步获取按下哪个按钮
             var result = await configDialog.ShowAsync();
 
+            // 如果按下了Primary
             if (result == ContentDialogResult.Primary)
             {
+                // 将ConfigIDTemp写入到当前配置ID下的localSettings
                 localSettings.Values["ConfigID" + ConfigIDNum] = localSettings.Values["ConfigIDTemp"];
+                // 刷新UI
                 refresh(ConfigIDNum);
             }
         }
@@ -346,13 +399,18 @@ namespace WinWoL
         private void RefConfigButton_Click(object sender, RoutedEventArgs e)
         {
             string ConfigIDNum = configNum.SelectedItem.ToString();
+            // Ping测试
             PingRDPRef(ConfigIDNum);
         }
         // 删除配置按钮点击
         private void DelConfigButton_Click(object sender, RoutedEventArgs e)
         {
-            delConfig(configNum.SelectedItem.ToString());
-            refresh(configNum.SelectedItem.ToString());
+            string ConfigIDNum = configNum.SelectedItem.ToString();
+            // 删除配置
+            delConfig(ConfigIDNum);
+            // 刷新UI
+            refresh(ConfigIDNum);
+            // 隐藏提示Flyout
             if (this.DelConfig.Flyout is Flyout f)
             {
                 f.Hide();
@@ -362,23 +420,20 @@ namespace WinWoL
         private void WoLConfigButton_Click(object sender, RoutedEventArgs e)
         {
             string ConfigIDNum = configNum.SelectedItem.ToString();
+            // WoL配置指定的设备
             WoLPC(ConfigIDNum);
         }
         // 远程桌面按钮点击
         private void RDPConfigButton_Click(object sender, RoutedEventArgs e)
         {
             string ConfigIDNum = configNum.SelectedItem.ToString();
+            // 刷新RDP主机端口的Ping状态
             PingRDPRef(ConfigIDNum);
-            if (RDPPing.Text != "RDP 端口延迟：无法联通")
-            {
-                ThreadStart childref = new ThreadStart(RDPPCChildThread);
-                Thread childThread = new Thread(childref);
-                childThread.Start();
-            }
-            else
-            {
-                RDPNotWorkTips.IsOpen = true;
-            }
+
+            // 启动一个子线程，防止UI卡死
+            ThreadStart childref = new ThreadStart(RDPPCChildThread);
+            Thread childThread = new Thread(childref);
+            childThread.Start();
         }
     }
 }
