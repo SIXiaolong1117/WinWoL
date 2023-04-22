ecb2ae56e4af3cb5d98c9d136bd116b84e52696b
SI_Xiaolong <sixiaolong2018@outlook.com>
2023-04-22 21:55:05 +0800

    导出配置的文件名默认包含配置名称。

--- WinWoL/WoL.xaml.cs
+++ WinWoL/WoL.xaml.cs
@@ -507,59 +507,73 @@ namespace WinWoL
         // 导出配置按钮点击
         private async void ExportConfig_Click(object sender, RoutedEventArgs e)
         {
+            // 从localSettings中读取字符串
             string ConfigIDNum = configNum.SelectedItem.ToString();
-            string configContent = localSettings.Values["ConfigID" + ConfigIDNum].ToString();
-
-            // 创建一个FileSavePicker
-            FileSavePicker savePicker = new Windows.Storage.Pickers.FileSavePicker();
-            // 获取当前窗口句柄 (HWND) 
-            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(App.m_window);
-            // 使用窗口句柄 (HWND) 初始化FileSavePicker
-            WinRT.Interop.InitializeWithWindow.Initialize(savePicker, hWnd);
-
-            // 为FilePicker设置选项
-            savePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
-            // 用户可以将文件另存为的文件类型下拉列表
-            savePicker.FileTypeChoices.Add("Plain Text", new List<string>() { ".wolconfig" });
-            // 默认文件名
-            savePicker.SuggestedFileName = "WinUI_Wake_on_LAN_BackUp_" + DateTime.Now.ToString();
-
-            // 打开Picker供用户选择文件
-            StorageFile file = await savePicker.PickSaveFileAsync();
-            if (file != null)
+            string configInner = localSettings.Values["ConfigID" + ConfigIDNum] as string;
+            // 如果字符串非空
+            if (configInner != null)
             {
-                // 阻止更新文件的远程版本，直到我们完成更改并调用 CompleteUpdatesAsync。
-                CachedFileManager.DeferUpdates(file);
+                // 分割字符串
+                string[] configInnerSplit = configInner.Split(',');
+                // 传入的字符串结构：
+                // configName.Text + "," + macAddress.Text + ","
+                // + ipAddress.Text + "," + ipPort.Text + ","
+                // + rdpIsOpen.IsOn + "," + rdpIpAddress.Text + "," + rdpIpPort;
+                string configName = configInnerSplit[0];
 
-                // 写入文件
-                using (var stream = await file.OpenStreamForWriteAsync())
+                string configContent = localSettings.Values["ConfigID" + ConfigIDNum].ToString();
+
+                // 创建一个FileSavePicker
+                FileSavePicker savePicker = new Windows.Storage.Pickers.FileSavePicker();
+                // 获取当前窗口句柄 (HWND) 
+                var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(App.m_window);
+                // 使用窗口句柄 (HWND) 初始化FileSavePicker
+                WinRT.Interop.InitializeWithWindow.Initialize(savePicker, hWnd);
+
+                // 为FilePicker设置选项
+                savePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+                // 用户可以将文件另存为的文件类型下拉列表
+                savePicker.FileTypeChoices.Add("Plain Text", new List<string>() { ".wolconfig" });
+                // 默认文件名
+                savePicker.SuggestedFileName = configName + "_BackUp_" + DateTime.Now.ToString();
+
+                // 打开Picker供用户选择文件
+                StorageFile file = await savePicker.PickSaveFileAsync();
+                if (file != null)
                 {
-                    using (var tw = new StreamWriter(stream))
+                    // 阻止更新文件的远程版本，直到我们完成更改并调用 CompleteUpdatesAsync。
+                    CachedFileManager.DeferUpdates(file);
+
+                    // 写入文件
+                    using (var stream = await file.OpenStreamForWriteAsync())
                     {
-                        tw.WriteLine(configContent);
+                        using (var tw = new StreamWriter(stream))
+                        {
+                            tw.WriteLine(configContent);
+                        }
                     }
-                }
 
-                // 让Windows知道我们已完成文件更改，以便其他应用程序可以更新文件的远程版本。
-                // 完成更新可能需要Windows请求用户输入。
-                FileUpdateStatus status = await CachedFileManager.CompleteUpdatesAsync(file);
-                if (status == FileUpdateStatus.Complete)
-                {
-                    // 文件保存成功
-                    SaveConfigTips.Title = "保存成功！";
-                    SaveConfigTips.IsOpen = true;
-                }
-                else if (status == FileUpdateStatus.CompleteAndRenamed)
-                {
-                    // 文件重命名并保存成功
-                    SaveConfigTips.Title = "重命名并保存成功！";
-                    SaveConfigTips.IsOpen = true;
-                }
-                else
-                {
-                    // 文件无法保存！
-                    SaveConfigTips.Title = "无法保存！";
-                    SaveConfigTips.IsOpen = true;
+                    // 让Windows知道我们已完成文件更改，以便其他应用程序可以更新文件的远程版本。
+                    // 完成更新可能需要Windows请求用户输入。
+                    FileUpdateStatus status = await CachedFileManager.CompleteUpdatesAsync(file);
+                    if (status == FileUpdateStatus.Complete)
+                    {
+                        // 文件保存成功
+                        SaveConfigTips.Title = "保存成功！";
+                        SaveConfigTips.IsOpen = true;
+                    }
+                    else if (status == FileUpdateStatus.CompleteAndRenamed)
+                    {
+                        // 文件重命名并保存成功
+                        SaveConfigTips.Title = "重命名并保存成功！";
+                        SaveConfigTips.IsOpen = true;
+                    }
+                    else
+                    {
+                        // 文件无法保存！
+                        SaveConfigTips.Title = "无法保存！";
+                        SaveConfigTips.IsOpen = true;
+                    }
                 }
             }
         }
