90417bf3f0359110ef4009ccdefe58eb94134486
SI Xiaolong <SIXiaolong_GitHub@outlook.com>
2023-10-09 21:42:28 +0800

    修改赞助人展示窗口的样式。

--- WinWoL/Pages/About.xaml.cs
+++ WinWoL/Pages/About.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI.Dispatching;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -9,7 +10,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -18,6 +21,7 @@ namespace WinWoL.Pages
 {
     public sealed partial class About : Page
     {
+        private DispatcherQueue _dispatcherQueue;
         public About()
         {
             this.InitializeComponent();
@@ -25,9 +29,13 @@ namespace WinWoL.Pages
             // 在构造函数或其他适当位置设置版本号
             var package = Package.Current;
             var version = package.Id.Version;
+            // 获取UI线程的DispatcherQueue
+            _dispatcherQueue = DispatcherQueue.GetForCurrentThread();
 
             APPVersion.Content = $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
             APPVersion.NavigateUri = new System.Uri($"https://github.com/Direct5dom/WinWoL/releases/tag/{version.Major}.{version.Minor}.{version.Build}.{version.Revision}");
+
+            GetSponsorList();
         }
 
         private void AboutAliPay_Click(object sender, RoutedEventArgs e)
@@ -37,6 +45,33 @@ namespace WinWoL.Pages
         private void AboutWePay_Click(object sender, RoutedEventArgs e)
         {
             AboutWePayTips.IsOpen = true;
+        }
+        private void GetSponsorList()
+        {
+            // 在子线程中执行任务
+            Thread subThread = new Thread(new ThreadStart(async () =>
+            {
+                string nameList;
+                using (HttpClient client = new HttpClient())
+                {
+                    // 发起GET请求以获取文件内容
+                    HttpResponseMessage response = await client.GetAsync($"https://raw.githubusercontent.com/Direct5dom/Direct5dom/main/README/Sponsor/List");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // 从响应中读取文件内容
+                        nameList = await response.Content.ReadAsStringAsync();
+                    }
+                    else
+                    {
+                        nameList = "Unable to connect to Github";
+                    }
+                }
+                _dispatcherQueue.TryEnqueue(() =>
+                {
+                    NameList.Text = nameList;
+                });
+            }));
+            subThread.Start();
         }
     }
 }
